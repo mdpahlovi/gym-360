625d284442dce6d2660d9341b40761da37ca5a0a
MD Pahlovi <mdpahlovi07@gmail.com>
2022-09-28 22:55:08 +0600

    Fake Data Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./components/Home";
+
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="section-gap">
+            <h1 className="heading-text">Gym 360 | No Pain No Gains</h1>
+        </div>
+    );
+};
+
+export default Home;
